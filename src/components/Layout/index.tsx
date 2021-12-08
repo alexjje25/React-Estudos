08da4d039d06f85d67120908da1e98cd62bc48ef
alexjje25 <83323289+alexjje25@users.noreply.github.com>
2021-12-08 14:30:29 -0300

    Add files via upload

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Main from '../Main';
+
+// import { Container } from './styles';
+
+const Layout: React.FC = () => {
+  return (
+      <Container>
+        <Wrapper>
+            <Main />
+        </Wrapper>
+      </Container>
+  );
+}
+
+export default Layout;
